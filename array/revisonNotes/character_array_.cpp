dcbb42d5c68ee33cc363be45c1d9f3bafae4cf2d
sailendrachettri <sailendra9083@gmail.com>
2022-08-18 10:22:54 +0530

    reverse a string

---
+++ array/revisonNotes/character_array_.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+int getLength(char name[])
+{
+    int count = 0;
+    for(int i = 0; name[i] != '\0'; i++)
+    {
+        count++;
+    }
+
+    return count;
+}
+
+void swap(char &first, char &last)
+{
+    char temp = first;
+    first = last;
+    last = temp;
+}
+
+void reverseString(char name[], int len)
+{
+    for(int i = 0; i < len/2; i++)
+    {
+        swap(name[i], name[len-i-1]);
+    }    
+}
+
+int main()
+{
+    // cout << "Working fine :)" << endl;
+    char name[20];
+    cout << "Enter your name: " << endl;
+
+    cin >> name;
+    // name[3] = '\0';
+
+    cout << name << endl;
+    int len = getLength(name);
+    cout << "Length: " << len << endl;
+
+    reverseString(name, len);
+    cout << name << endl;
+
+    return 0;
+}
