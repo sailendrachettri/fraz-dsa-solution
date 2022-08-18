e5c5dc8cc4d37d9689ffe4af5c619259d055b1ea
sailendrachettri <sailendra9083@gmail.com>
2022-08-18 08:15:44 +0530

    fibonacci number

---
+++ array/fibonacci_number_509.cpp
@@ -0,0 +1,16 @@
+class Solution {
+    
+    int solve(int n)
+    {
+        if(n <= 1)
+            return n;
+        
+        return (solve(n-1) + solve(n-2) );        
+    }
+    
+public:
+    int fib(int n) {
+        
+        return solve(n)   ;
+    }
+};
