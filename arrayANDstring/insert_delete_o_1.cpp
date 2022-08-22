41a14cace57c3169cfcc8abd35e1da64a5df0e59
sailendrachettri <sailendra9083@gmail.com>
2022-08-22 11:58:00 +0530

    rename folder array to arrayANDstring

---
+++ arrayANDstring/insert_delete_o_1.cpp
@@ -0,0 +1,48 @@
+class RandomizedSet {
+    
+    vector<int> _nums;
+    unordered_map<int, int> _positions;
+    
+public:
+    RandomizedSet() {
+        
+    }
+    
+    bool insert(int val) {
+        if(_positions.find(val) != _positions.end() )
+            return false;
+        
+        _nums.push_back(val);
+        
+        _positions.insert({val, _nums.size()-1});
+        return true;
+    }
+    
+    bool remove(int val) {
+        if(_positions.find(val) == _positions.end() )
+            return false;
+        
+        int pos = _positions[val];
+        
+        _nums[pos] = _nums[_nums.size()-1];
+        _positions[_nums[pos] ] = pos;        
+        
+        _nums.pop_back();
+        _positions.erase(val);
+        
+        return true;
+    }
+    
+    int getRandom() {
+        
+        return _nums[rand() % _nums.size()];
+    }
+};
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet* obj = new RandomizedSet();
+ * bool param_1 = obj->insert(val);
+ * bool param_2 = obj->remove(val);
+ * int param_3 = obj->getRandom();
+ */
