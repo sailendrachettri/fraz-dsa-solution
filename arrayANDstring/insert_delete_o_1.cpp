5ad391991333b07eb7a531a00691ee8324296cbb
sailendrachettri <sailendra9083@gmail.com>
2022-08-22 11:58:45 +0530

    380. Insert Delete GetRandom O(1)

--- arrayANDstring/insert_delete_o_1.cpp
+++ arrayANDstring/insert_delete_o_1.cpp
@@ -38,11 +38,3 @@ public:
         return _nums[rand() % _nums.size()];
     }
 };
-
-/**
- * Your RandomizedSet object will be instantiated and called as such:
- * RandomizedSet* obj = new RandomizedSet();
- * bool param_1 = obj->insert(val);
- * bool param_2 = obj->remove(val);
- * int param_3 = obj->getRandom();
- */
